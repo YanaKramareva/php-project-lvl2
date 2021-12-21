3efbfa8f145bd72f5dd3fb40f991a819ec8749b0
YanaKramareva <iana2000@mail.ru>
2021-12-21 18:58:11 +0300

    Changed files: genDiff.php

--- src/genDiff.php
+++ src/genDiff.php
@@ -10,7 +10,7 @@ function makeArrayFromFile($file)
     return json_decode($arrayFromFile, true);
 }
 
-function zipFiles($file1, $file2)
+function zipFilesToArray($file1, $file2): array
 {
     $zipFilesArray = zip_all(makeArrayFromFile($file1), makeArrayFromFile($file2));
     uksort($zipFilesArray, function ($left, $right) {
@@ -19,30 +19,35 @@ function zipFiles($file1, $file2)
     return $zipFilesArray;
 }
 
+function boolToString(bool $item): string
+{
+    return $item === true ? 'true' : 'false';
+}
+
 function genDiff($file1, $file2): string
 {
-    $zipFilesArray = zipFiles($file1, $file2);
+    $zipFilesArray = zipFilesToArray($file1, $file2);
     $result = [];
     foreach ($zipFilesArray as $item => $value) {
-        if (is_bool($value[0])) {
-            $value[0] = ($value[0] === true ? 'true' : 'false');
-        }
-        if (is_bool($value[1])) {
-            $value[1] = ($value[1] === true ? 'true' : 'false');
-        }
-        if (is_null($value[0])) {
-            $result[] =  " + $item: $value[1]";
-        }
-        if (is_null($value[1])) {
-            $result[] = " - $item: $value[0]";
-        }
-        if ($value[0] === $value[1]) {
-            $result[] =  "   $item: $value[1]";
-        }
-        if ($value[0] !== $value[1] && $value[0] !== null && $value[1] !== null) {
-            $result[] = " - $item: $value[0]";
-            $result[] = " + $item: $value[1]";
-        }
+
+        $value[0] = (is_bool($value[0]) ? boolToString($value[0]) : $value[0]);
+        $value[1] = (is_bool($value[1]) ? boolToString($value[1]) : $value[1]);
+
+        switch ($value) {
+            case is_null($value[0]):
+                $result[] =  " + $item: $value[1]";
+                break;
+            case is_null($value[1]):
+                $result[] = " - $item: $value[0]";
+                break;
+            case $value[0] === $value[1]:
+                $result[] =  "   $item: $value[1]";
+                break;
+                case ($value[0]!== $value[1] && $value[0] !== null && $value[1] !== null):
+                    $result[] = " - $item: $value[0]";
+                    $result[] = " + $item: $value[1]";
+                    break;
+                    }
     }
     return implode(PHP_EOL, $result);
 }
