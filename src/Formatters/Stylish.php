b6f6169e789faec1297a2a71e97bb6fd36c82ab7
YanaKramareva <iana2000@mail.ru>
2022-01-27 13:03:11 +0300

    Changed Stylish.php

--- src/Formatters/Stylish.php
+++ src/Formatters/Stylish.php
@@ -32,14 +32,11 @@ function getBlock($item, $level)
         $beforeValue = formatValue($item['beforeValue'], $level + 1);
         $afterValue = formatValue($item['afterValue'], $level + 1);
         if ($beforeValue === '') {
-            return "{$spaces}  - {$key}:\n" .
-                "{$spaces}  + {$key}: {$afterValue}";
+            return "{$spaces}  - {$key}:{$beforeValue}\n" . "{$spaces}  + {$key}: {$afterValue}";
         } elseif ($beforeValue === ' ') {
-            return "{$spaces}  - {$key}: \n" .
-                "{$spaces}  + {$key}: {$afterValue}";
+            return "{$spaces}  - {$key}:{$beforeValue}\n" . "{$spaces}  + {$key}: {$afterValue}";
         } else {
-            return "{$spaces}  - {$key}: {$beforeValue}\n" .
-                "{$spaces}  + {$key}: {$afterValue}";
+            return "{$spaces}  - {$key}: {$beforeValue}\n" . "{$spaces}  + {$key}: {$afterValue}";
         }
     }
 
