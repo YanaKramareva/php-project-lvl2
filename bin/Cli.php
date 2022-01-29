5ae52d54a2e67514fc2973062ed125d10e0f398a
YanaKramareva <iana2000@mail.ru>
2022-01-29 18:48:32 +0300

    Added Cli.php

---
+++ bin/Cli.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Differ\Cli;
+
+use Docopt;
+
+use function Differ\Differ\genDiff;
+
+function start()
+{
+    $doc = <<<'DOCOPT'
+    Generate diff
+    Usage:
+      gendiff (-h|--help)
+      gendiff (-v|--version)
+      gendiff [--format <fmt>] <firstFile> <secondFile>
+    Options:
+      -h --help                     Show this screen
+      -v --version                  Show version
+      --format <fmt>                Report format [default: stylish]
+    DOCOPT;
+
+    $args = Docopt::handle($doc, ['version' => '1.0']);
+
+    $diff = genDiff(
+        $args['<firstFile>'],
+        $args['<secondFile>'],
+        $args['--format']
+    );
+    print_r($diff);
+}
